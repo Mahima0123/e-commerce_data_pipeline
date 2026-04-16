ac85810278071501d5e09d3e360e0655346727a0
Mahima Chaudhary <chaudharymahima983@gmail.com>
2026-04-15 22:46:35 -0500

    data transformation lowercase issue fix

--- transform_data.py
+++ transform_data.py
@@ -6,11 +6,14 @@ engine = create_engine("postgresql://postgres@localhost:5432/ecommerce_db")
 # read data from database
 orders = pd.read_sql("SELECT * FROM orders", engine)
 
+orders["status"] = orders["status"].str.lower()
+
 # select other data except cancelled orders
 orders_cleaned = orders[orders["status"] != "cancelled"].copy()
 
-# standardize to lowercase
-orders_cleaned.loc[:, "status"] = orders_cleaned["status"].str.lower()
-
 # add new column(amount > 300)
 orders_cleaned.loc[:, "high_value"] = orders_cleaned["amount"] > 300
+
+# save to database
+orders_cleaned.to_sql("orders_cleaned", engine, if_exists="replace", index=False)
+print("Data transformed and saved successfully!")
